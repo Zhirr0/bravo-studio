666566052d4113bc1dfd44ccfc00d374228484fd
Zhir Bakhtyar <zheerbaxtyar9@gmail.com>
2026-04-09 14:29:50 +0300

    testing it with no large data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { connection } from "next/server";
 import PageReadySignal from "@/components/PageReadySignal";
 import Catalyst from "@/components/sections/CatalystSection";
 import FolioSection from "@/components/sections/FolioSection";
@@ -8,9 +7,7 @@ import OurVoice from "@/components/sections/OurVoiceSection";
 import ParallelSection from "@/components/sections/ParallelSection";
 import SharedShader from "@/components/sections/SharedShaderSection";
 
-export default async function Home() {
-  await connection(); // tells the website to not cache data, we are gonna keep this for now to see how long loading look like
-  await new Promise((r) => setTimeout(r, 3000));
+export default function Home() {
 
   return (
     <div className="home page flex flex-row gap-10">
